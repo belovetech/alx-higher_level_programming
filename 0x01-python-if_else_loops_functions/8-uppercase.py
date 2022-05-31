5223a3fdd8823e600336911a5beaac589d6c632c
belovetech <belovetech@gmail.com>
2022-05-31 22:25:00 +0100

    Refactor code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     for ch in str:
         """Change to uppercase character."""
         if ord(ch) >= 97 and ord(ch) <= 123:
-            print("{}".format(chr(ord(ch) - 32)), end="")
-        else:
-            print("{}".format(chr(ord(ch))), end="")
+            ch = chr(ord(ch) - 32)
+        print("{}".format(ch), end="")
     print()

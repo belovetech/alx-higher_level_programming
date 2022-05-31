760d0befca6c529ac886a5cc4f9e4139127a39f8
belovetech <belovetech@gmail.com>
2022-05-31 19:54:12 +0100

    Corrected ascii value to skip

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
-    if ch == 99 or ch == 113:
+    if ch == 101 or ch == 113:
         continue
     else:
         print("{}".format(chr(ch)), end="")

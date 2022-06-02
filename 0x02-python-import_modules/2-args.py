1c174ec3c156da827b561cced0d703f1458c803c
belovetech <belovetech@gmail.com>
2022-06-02 21:14:00 +0100

    List te number of arguement

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number and list of arguements."""
+    import sys
+
+    argc = len(sys.argv) - 1
+
+    print("{}: arguements".format(argc))
+
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, sys.argv[i]))

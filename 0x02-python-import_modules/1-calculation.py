d915403140254569c21c30c4a87f0d86463bec68
belovetech <belovetech@gmail.com>
2022-06-02 20:53:19 +0100

    fix bug

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-if __name__ == '__main___':
+if __name__ == "__main__":
     """Print the sum, diff, multiple and quotient of 10 and 5"""
     from calculator_1 import add, sub, mul, div
 
@@ -8,6 +8,6 @@ if __name__ == '__main___':
     b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} + {} = {}".format(a, b, sub(a, b)))
-    print("{} + {} = {}".format(a, b, mul(a, b)))
-    print("{} + {} = {}".format(a, b, div(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))

024d107bebc4b142ae8758a56f4c6a953291d661
belovetech <belovetech@gmail.com>
2022-06-03 06:43:16 +0100

    fix bug

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,18 +3,22 @@
 if __name__ == "__main__":
     """Handle basic arithemetic."""
     from calculator_1 import add, sub, mul, div
-    import sys
+    from sys import argv
 
-    argc = len(sys.argv) - 1
+    argc = len(argv) - 1
+
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    operators = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(operators.keys()):
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+
+    if op not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
+        print("{} {} {} = {}".format(a, op, b, ops[op](a, b)))

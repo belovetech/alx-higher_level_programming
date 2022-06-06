fc23ed6ec8563565f7c20d59d21ca5b85b95a5c1
belovetech <belovetech@gmail.com>
2022-06-06 20:41:09 +0100

    Fix bug

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,7 +14,7 @@ int is_palindrome(listint_t **head)
 	unsigned int len = 0, i = 0, flag = 1, lastidx, *buf;
 
 	current = *head;
-	if (*head == NULL || *(head)->next == NULL)
+	if (*head == NULL || head == NULL)
 		return (1);
 
 	while (current != NULL)

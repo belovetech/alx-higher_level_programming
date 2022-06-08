29440e60d99455e5c7e778ee9169888058716763
belovetech <belovetech@gmail.com>
2022-06-08 14:04:07 +0100

    Use intersection method to find common element

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -6,5 +6,6 @@ from hashlib import new
 
 def common_elements(set_1, set_2):
     """Returns a set of common elements in two sets."""
-    new_set = set_1 & set_2
+    # new_set = set_1 & set_2
+    new_set = set_1.intersection(set_2)
     return new_set

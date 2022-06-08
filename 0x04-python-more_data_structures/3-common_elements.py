5d9ade9362fe90c270d92560199fba22891aa54e
belovetech <belovetech@gmail.com>
2022-06-08 13:58:14 +0100

    Common element in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+from hashlib import new
+
+
+def common_elements(set_1, set_2):
+    """Returns a set of common elements in two sets."""
+    new_set = set_1 & set_2
+    return new_set

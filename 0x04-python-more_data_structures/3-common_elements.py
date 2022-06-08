d014b28f36b0126626622116463103f7f8b4f99a
belovetech <belovetech@gmail.com>
2022-06-08 14:15:31 +0100

    Use for loop to find common element

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -7,5 +7,10 @@ from hashlib import new
 def common_elements(set_1, set_2):
     """Returns a set of common elements in two sets."""
     # new_set = set_1 & set_2
-    new_set = set_1.intersection(set_2)
+    # new_set = set_1.intersection(set_2)
+    new_set = []
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                new_set.append(i)
     return new_set

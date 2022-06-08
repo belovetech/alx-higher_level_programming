b1d0bc6ec05ac144202db0b6e012d39dc0305ce2
belovetech <belovetech@gmail.com>
2022-06-08 14:35:57 +0100

    Use another approach

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,11 +2,19 @@
 
 def common_elements(set_1, set_2):
     """Returns a set of common elements in two sets."""
-    new_set = set_1 & set_2
-    # new_set = set_1.intersection(set_2)
-    # new_set = []
+    # first approach
+    # common_set = set_1 & set_2
+    # second approach
+    # common_set = set_1.intersection(set_2)
+    # third approach
+    # common_set = []
     # for i in set_1:
     #     for j in set_2:
     #         if i == j:
-    #             new_set.append(i)
-    return new_set
+    #             common_set.append(i)
+    # fourth approach
+    common_set = set()
+    for x in set_1:
+        if x in set_2:
+            common_set.add(x)
+    return common_set

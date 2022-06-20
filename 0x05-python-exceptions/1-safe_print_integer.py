f92d4a0d6e254e05e4f8d235116b99b198688224
belovetech <belovetech@gmail.com>
2022-06-20 13:35:05 +0100

    Use else to return value

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,7 @@ def safe_print_integer(value):
     """prints an integer with "{:d}".format()"""
     try:
         print("{:d}".format(value))
-        return True
     except ValueError:
         return False
+    else:
+        return True

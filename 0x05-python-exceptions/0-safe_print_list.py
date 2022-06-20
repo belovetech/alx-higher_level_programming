8dc7e55bac8a6ac1d7618031eec154cb1155c00e
belovetech <belovetech@gmail.com>
2022-06-20 13:01:06 +0100

    Prints x element of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """prints x elements of a list"""
+    try:
+        length = 0
+        new_list = my_list[:x]
+
+        for x in new_list:
+            length += 1
+            print(x, end="")
+        print()
+
+    except:
+        print("Error")
+
+    else:
+        return length

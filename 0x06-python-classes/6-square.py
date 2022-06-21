5cd9ef814e78ae84246e16e442c6d92cb4b49837
belovetech <belovetech@gmail.com>
2022-06-21 22:31:10 +0100

    Add getter and setter of position method

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+"""Defines a square."""
+
+
+from pyrsistent import v
+
+
+class Square:
+    """Representation of Square class"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a square
+
+        Args:
+            size (int): size of the square
+            position (tuple): tuple of 2 positive integers
+        """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Get/set size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+            not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise ValueError(
+                "position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Finds area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+            return
+
+        for i in range(self.__position[1]):
+            print()
+
+        for x in range(self.__size):
+            for y in range(self.__position[0]):
+                print("{}".format(" "), end="")
+            for z in range(self.__size):
+                print("{}".format("#"), end="")
+            print()

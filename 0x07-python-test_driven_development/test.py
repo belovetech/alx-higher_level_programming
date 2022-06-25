845f3b60bee61da5f1596f12fea762245b6f11f1
belovetech <belovetech@gmail.com>
2022-06-25 19:08:08 +0100

    Add two integers and differnet test cases

---
+++ 0x07-python-test_driven_development/test.py
@@ -0,0 +1,13 @@
+# x = 10.30
+# y = 5.00
+
+# x = int(x)
+# y = int(y)
+
+# print(x + y)
+
+pos_ifnity = float('inf')
+neg_ifnity = float('-inf')
+
+print("Positive", pos_ifnity)
+print("Negative", neg_ifnity)

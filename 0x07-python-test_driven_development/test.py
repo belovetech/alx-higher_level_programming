f5c5d1cc60af3d71487d3d7467dc37a80f3b0821
belovetech <belovetech@gmail.com>
2022-06-27 21:54:28 +0100

    Remove unwanted directory

--- 0x07-python-test_driven_development/test.py
+++ 0x07-python-test_driven_development/test.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
 matrix = [
-    [1, 2.3, 3],
+    [1, 2.3, 3, 6],
     [4, 5.2, 6.7],
-    [4, 5.2, 6.7, 5],
-    [4, 5.2, 6.7]
+    [4, 5.2, 6.7],
+    [4, 5.2, 6.7, 6]
 ]
-# new_list = []
-
-len1 = len(matrix[0])
-flag = 0
-for row in matrix:
-    if len(row) != len1:
-        flag = 0
-        break
-    else:
-        flag = 1
 
 
-if flag:
-    print("Equal")
+iterable = iter(matrix)
+length = len(next(iterable))
+# print(length)
+# for l in iterable:
+#     print(len(l))
+# print(len(next(iterable)))
+
+if all(len(l) == length for l in iterable):
+    print("The same!")
 else:
-    print("Not equal")
+    print("Not the same")
 
+
+# for i in range(len(matrix)):
+#     print(next(len(x)))
+
+# len1 = len(matrix[0])
+# flag = 0
+# for row in matrix:
+#     if len(row) != len1:
+#         flag = 0
+#         break
+#     else:
+#         flag = 1
+
+# if flag:
+#     print("Equal")
+# else:
+#     print("Not equal")
 
 # for i in range(len(matrix)):
 #     if len(matrix[i]) == len(matrix[i+1]):

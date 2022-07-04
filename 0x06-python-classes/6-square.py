33c7f16931cf047d4b1a99b960d903b5fd4d5f84
belovetech <belovetech@gmail.com>
2022-07-04 11:27:05 +0530

    Update docstring in the square class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,9 @@ class Square:
     """Representation of a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a square
+        """A class with private instance attributes.
+        def __init__: initialize class Square.
+
         Args:
             size (int): size of the square
             position (tuple): position of the square
@@ -16,7 +18,21 @@ class Square:
 
     @property
     def size(self):
-        """Get/Set size of the square."""
+        """property getter: retrive the value of size.
+
+        Returns:
+            int: the set instance attribute.
+
+        size.setter: set the value of self.__size.
+        It also checks for TypeError and ValueError.
+
+        Args:
+            value (int): value to initialize self.__size with.
+
+        Raises:
+            TypeError: if value is not an integer.
+            ValueError: if value is lesser than 0.
+        """
         return self.__size
 
     @size.setter
@@ -30,7 +46,20 @@ class Square:
 
     @property
     def position(self):
-        """Get/Set position of the square."""
+        """property getter: retrive the value of position.
+
+        Returns:
+            int: the set instance attribute.
+
+        position.setter: set the value of self.__position.
+        It also checks for TypeError.
+
+        Args:
+            value (tuple): value to initialize self.__position with.
+
+        Raises:
+            TypeError: if value is not a tuple of 2 positive integers.
+        """
         return self.__position
 
     @position.setter
@@ -43,7 +72,11 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Return area of the square."""
+        """calculates the area of the current square.
+
+        Returns:
+            int: the area of the square.
+        """
         return self.__size * self.__size
 
     def my_print(self):
@@ -59,6 +92,7 @@ class Square:
         for x in range(self.__size):
             for y in range(self.__position[0]):
                 print(" ", end="")
-            for z in range(self.__size):
-                print("#", end="")
-            print("")
+            print("#" * self.__size)
+            # for z in range(self.__size):
+            #     print("#", end="")
+            # print("")

8880180fb9e461e9d6327d051d604d7975f4e9f1
belovetech <belovetech@gmail.com>
2022-07-04 13:12:14 +0100

    Look up attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Function that returns the list of available attributes and methods of an object:
+"""Function that returns the list of
+    available attributes and methods of an object:
 """
 
 

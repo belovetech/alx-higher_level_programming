96ec48d1700747e8062823438a2288e5b6612584
belovetech <belovetech@gmail.com>
2022-07-04 20:04:41 +0100

    Fix pycodestyle error

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Check if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class 
+Check if the object is an instance of, or if the object
+is an instance of a class that inherited from, the specified class
 """
 
 
@@ -12,7 +13,7 @@ def is_kind_of_class(obj, a_class):
         a_class (class): a specific class
 
     Returns:
-        True: if the object is an instance of, or if the object is an instance of a class that inherited from
+        True: Same class or inherit from
         False: Not the same
     """
     if isinstance(obj, a_class):

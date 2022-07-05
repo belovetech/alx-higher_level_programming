5ba9ac3e4525cca5a4dac7476503c9dbbdba1e73
belovetech <belovetech@gmail.com>
2022-07-05 18:43:23 +0100

    Write to a file and retuns number of charcter written

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -17,5 +17,5 @@ def write_file(filename="", text=""):
     Returns:
         number of characters written
     """
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, mode="w", encoding="utf-8") as file:
         return file.write(text)

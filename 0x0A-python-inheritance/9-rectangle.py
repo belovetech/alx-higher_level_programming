c94619b2423571d37074991f0584a26b52362d83
belovetch <belovetech@gmail.com>
2022-07-05 06:30:36 +0100

    Fix pycodestyle error

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -5,27 +5,27 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-	"""class Rectangle that inherits from BaseGeometry
-	Subclass is initialized with args
-	Args:
-		width (int): width of the subclass Rectangle
-		height (int): heigth of the subclass Rectangle
-	"""
+    """class Rectangle that inherits from BaseGeometry
+    Subclass is initialized with args
+    Args:
+        width (int): width of the subclass Rectangle
+        height (int): heigth of the subclass Rectangle
+    """
 
-	def __init__(self, width, height):
+    def __init__(self, width, height):
 
-		self.integer_validator("width", width)
-		self.integer_validator("height", height)
-		self.__width = width
-		self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
-	def area(self):
-		return self.__width * self.__height
+    def area(self):
+        return self.__width * self.__height
 
-	def __str__(self):
-		"""String representatin of area value"""
-		#return "[Rectangle] {:d}/{:d}".
-		# format(self.__width, self.__height), end=""
-		print("[Rectangle] {:d}/{:d}".format(self.__width, self.__height), end="")
-		return ""
-		
+    def __str__(self):
+        """String representatin of area value"""
+        # return "[Rectangle] {:d}/{:d}"
+        # .format(self.__width, self.__height), end=""
+        print("[Rectangle] {:d}/{:d}"
+              .format(self.__width, self.__height), end="")
+        return ""

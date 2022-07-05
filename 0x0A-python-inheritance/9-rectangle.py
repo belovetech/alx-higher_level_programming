337e3d544899b717e250df78d305236439214fef
belovetch <belovetech@gmail.com>
2022-07-05 05:08:49 +0100

    Compute area of rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""A subclass of class BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry
+    Subclass is initialized with args
+
+    Args:
+        width (int): width of the subclass Rectangle
+        height (int): heigth of the subclass Rectangle
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = width
+    
+    def area(self):
+        """Computes the area of rectangle"""
+        return (self.__width * self.__height)
+        
+    def __str__(self):
+        """String representatin of area value"""
+        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
+        

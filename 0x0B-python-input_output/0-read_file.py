3ffd37cfc8064b72f54cdbd476655410853298f7
belovetech <belovetech@gmail.com>
2022-07-05 18:19:57 +0100

    Read file and print to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """
-reads a text file (UTF8) and prints it to stdout.
+reads a text file
 """
 
 
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout."""
+    """reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename(str): name of the file to read
+    """
 
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        read_file = file.read()
+        print(read_file)

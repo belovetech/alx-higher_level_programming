aa5e3e37dea0be25299117b87a771e1fd1d536f7
belovetch <belovetech@gmail.com>
2022-07-05 05:17:41 +0100

    Compute area of rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -25,5 +25,8 @@ class Rectangle(BaseGeometry):
         
     def __str__(self):
         """String representatin of area value"""
-        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height), end="")
+        # return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height), end=""
+        print("[Rectangle] {:d}/{:d}".format(self.__width, self.__height), end="")
+        return ""
+
         

c7c5ab1330d080ab538416391226c854de8931ec
belovetch <belovetech@gmail.com>
2022-07-05 06:34:42 +0100

    Remove trailling whitespaces

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -24,4 +24,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-            

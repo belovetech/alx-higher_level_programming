92f5e04f57a1adf6927c9b22034d6b582b799a69
belovetech <belovetech@gmail.com>
2022-07-10 09:14:30 +0100

    Typecast the class attributes to its type

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,5 +16,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
+            type(self).__nb_objects += 1
             self.id = self.__nb_objects

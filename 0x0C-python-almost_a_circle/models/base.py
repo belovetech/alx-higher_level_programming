a59f1d659a5b746698bf85beb42a04541191cbed
belovetech <belovetech@gmail.com>
2022-07-12 17:17:19 +0100

    Refactor to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,8 +33,8 @@ class Base:
         Returns:
             JSON string representation of list_dictionaries
         """
-        if list_dictionaries is None or list_dictionaries == "[]":
-            return []
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 

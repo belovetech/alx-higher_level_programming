d3638c36e5aedc16844031930b0836c26e0a82b5
belovetech <belovetech@gmail.com>
2022-08-11 04:01:39 +0530

    Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,6 @@
--- Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows AS s
-JOIN tv_show_genres AS g
-ON s.id = g.show_id
-ORDER BY s.title, g.genre_id;
+-- Script that lists all cities contained in the database
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
 

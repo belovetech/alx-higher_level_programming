43f8820a7056236c066bac629463d627ae0ef4e8
belovetech <belovetech@gmail.com>
2022-09-05 14:29:21 +0100

    Check if first argument is not a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 
-const argv = process.argv;
-console.log(
-  `${isNaN(argv[2]) ? 'Not a number' : `My number: ${Math.trunc(argv[2])}`} `
-);
+const num = Math.trunc(process.argv[2]);
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);

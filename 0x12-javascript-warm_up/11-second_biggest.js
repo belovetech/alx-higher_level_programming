f315bfa0fc1ca1658d5f9d62ebe2c4809e73d715
belovetech <belovetech@gmail.com>
2022-09-05 19:13:50 +0100

    Refactor the code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,29 +2,25 @@
 
 const argv = process.argv;
 
-// Extract only numbers
-let nums = argv.filter(cur => !isNaN(cur));
-
-if (!nums[0] || nums.length === 1) {
-  console.log(0);
-  return 0;
-}
-
-// Convert them to integers
-nums = nums.map(cur => parseInt(cur));
+// Extract only Integers
+const numsArgs = argv.filter(cur => !isNaN(cur)).map(cur => parseInt(cur));
+if (!numsArgs[0] || numsArgs.length === 1) console.log(0);
 
 // find max in array
 const findMax = function (nums) {
   let max = nums[0];
   for (const num of nums) {
-    if (max > num) max = max;
-    else max = num;
+    if (max < num) max = num;
   }
   return max;
 };
 
-// Remove the first biggest
-const newArr = nums.filter((cur, _, arr) => cur !== findMax(arr));
+const findSecondBiggest = function (nums) {
+  if (nums.length > 1) {
+    // Remove the first biggest
+    const newArr = nums.filter((cur, _, arr) => cur !== findMax(arr));
+    console.log(findMax(newArr));
+  }
+};
 
-// find second biggest
-console.log(findMax(newArr));
+findSecondBiggest(numsArgs);

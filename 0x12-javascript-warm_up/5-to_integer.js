24588dc7971615638d7d11b90ba08b9a42601717
belovetech <belovetech@gmail.com>
2022-09-05 14:18:32 +0100

    Print my number if first argument can be converted to an integer otherwise NaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 
 const argv = process.argv;
 console.log(
-  `${argv[2] ? `My number: ${Math.trunc(argv[2])}` : 'Not a number'} `
+  `${isNaN(argv[2]) ? 'My number' : `My number: ${Math.trunc(argv[2])}`} `
 );

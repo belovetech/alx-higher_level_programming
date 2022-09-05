38638e5fd252c7550873f04574af4cc503d74b8c
belovetech <belovetech@gmail.com>
2022-09-05 13:51:47 +0100

    Print the first argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-const argv = process.argv;
-
-argv[0] && argv[1] && !argv[2]
-  ? console.log('No argument')
-  : console.log(argv[2]);
+console.log(!process.argv[2] ? 'No argument' : process.argv[2]);

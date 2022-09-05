f3a32b299b29280de2b2ee29bb7215b49d5d62da
belovetech <belovetech@gmail.com>
2022-09-05 13:05:01 +0100

    Fix code bugs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,6 @@
 
 const argc = process.argv.length;
 
-if (argc <= 2) console.log('No arguement');
 if (argc === 3) console.log('Arguement found');
-if (argc > 3) console.log('Arguements found');
+else if (argc > 3) console.log('Arguements found');
+else console.log('No arguement');

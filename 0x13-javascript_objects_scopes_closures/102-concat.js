23c4f34a5a379bd07ca8cd38fee2478412f01d32
belovetech <belovetech@gmail.com>
2022-09-06 19:21:43 +0100

    Try synchronize way

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
 const argv = process.argv;
 
+const file1 = fs.readFileSync(argv[2], 'utf-8').toString();
+const file2 = fs.readFileSync(argv[3], 'utf-8').toString();
+fs.writeFileSync(argv[4], file1 + file2);
+
+/*
 fs.readFile(argv[2], 'utf8', function (err, data1) {
   if (err) throw err;
 
@@ -15,3 +19,4 @@ fs.readFile(argv[2], 'utf8', function (err, data1) {
     });
   });
 });
+*/

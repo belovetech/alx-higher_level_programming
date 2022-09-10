585a3d869bec868274d68943e38f2aaba3554198
belovetech <belovetech@gmail.com>
2022-09-10 16:00:02 +0100

    Fix pycodestyle error

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+"""Script that lists all State objects that contain
+ the letter a from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
@@ -14,7 +15,8 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.ilike('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(State.name.ilike('%a%')).
+    order_by(State.id).all()
 
     for state in states:
         print('{}: {}'.format(state.id, state.name))

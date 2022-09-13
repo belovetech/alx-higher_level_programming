df75f17392cad09bae2a606c1956499bde53bb29
belovetech <belovetech@gmail.com>
2022-09-13 04:24:58 +0100

    Script that lists all State objects, and corresponding City objects

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -19,10 +19,9 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state_city = session.query(State)
-    .outerjoin(City).order_by(State.id, City.id).all()
+    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
 
-    for state in state_city:
+    for state in st:
         print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
             print('    {}: {}'.format(city.id, city.name))

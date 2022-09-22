58ea5453969ab50d26bcffcacba731b2f00cf8c5
belovetech <belovetech@gmail.com>
2022-09-22 22:30:27 +0100

    Send a POST request to the passed URL, and displays the body of the response

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script that takes in a URL as an argument, sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST  -d "email=test@gmail.com&subject=
+I will always be here for PLD"

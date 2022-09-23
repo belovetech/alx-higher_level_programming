60ae4c6f98e538889ae8dc4a3fde76c1ee61d900
belovetech <belovetech@gmail.com>
2022-09-23 12:38:17 +0100

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,8 @@ import urllib.request
 
 
 if __name__ == '__main__':
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+    # req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))

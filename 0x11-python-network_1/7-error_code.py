778081cdc028afb082f30c5816d51aa8b91fca15
belovetech <belovetech@gmail.com>
2022-09-23 20:59:59 +0100

    print: Error code: followed by the value of the HTTP status code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and
+displays the body of the response.
+If the HTTP status code is greater than or equal to 400,
+print: Error code: followed by the value of the HTTP status code"""
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    bodyRes = requests.get(sys.argv[1])
+    if bodyRes.status_code >= 400:
+        print('Status code: {}'.format(bodyRes.status_code))
+    else:
+        print(bodyRes.text)

63e163e3500de955635e9c90c0ab4878e0210b70
belovetech <belovetech@gmail.com>
2022-09-23 20:31:42 +0100

    Remove unwanted import of package

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,7 +2,6 @@
 """Python script that takes in a URL and an email address,
 sends a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response."""
-import email
 import sys
 import requests
 

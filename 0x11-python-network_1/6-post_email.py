6defb34563cf80b498babdd36730348f0d33d5f0
belovetech <belovetech@gmail.com>
2022-09-23 20:30:47 +0100

    Send a POST request to the passed URL with the email as a parameter

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response."""
+import email
+import sys
+import requests
+
+if __name__ == '__main__':
+    data = {'email': sys.argv2}
+    bodyRes = requests.post(sys.argv[1], data=data)
+    print(bodyRes.text)

0cedd95e62659d7904555a083668664b8a4081d3
belovetech <belovetech@gmail.com>
2022-09-24 06:00:10 +0100

    Display your github id using github API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,11 +6,17 @@ import sys
 import requests
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    auth = requests.auth.HTTPBasicAuth(username, password)
-    url = 'https://api.github.com/user'
+    # username = sys.argv[1]
+    # password = sys.argv[2]
+    # auth = requests.auth.HTTPBasicAuth(username, password)
+    # url = 'https://api.github.com/user'
 
-    res = requests.post(url, auth=auth)
-    res_json = res.json()
-    print(res_json.get('id'))
+    # res = requests.post(url, auth=auth)
+    # res_json = res.json()
+    # print(res_json.get('id'))
+
+    url = "https://api.github.com/user"
+    response = requests.get(
+        url, auth=requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    res_dict = response.json()
+    print(res_dict.get('id'))

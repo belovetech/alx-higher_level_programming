30a8cdf460cd4d7d4b226080f9e6af3b871deb92
belovetech <belovetech@gmail.com>
2022-10-18 17:35:52 +0100

    [Feat]: script that gets the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,9 +3,7 @@
 const fs = require('fs');
 const request = require('request');
 
-const url = process.argv[2];
-
-request(url, (err, res, body) => {
+request(process.argv[2], (err, res, body) => {
   if (err) console.log(err);
 
   fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
